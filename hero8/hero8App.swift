6be3901b253db7a47a7dc7fdbacd197a8c8f0305
Lanadya <klenin@gmx.de>
2025-04-18 18:25:50 +0200

    Initial Commit

---
+++ hero8/hero8App.swift
@@ -0,0 +1,17 @@
+//
+//  hero8App.swift
+//  hero8
+//
+//  Created by Nina Klee on 18.04.25.
+//
+
+import SwiftUI
+
+@main
+struct hero8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hero8Tests/hero8Tests.swift
@@ -0,0 +1,16 @@
+//
+//  hero8Tests.swift
+//  hero8Tests
+//
+//  Created by Nina Klee on 18.04.25.
+//
+
+import Testing
+
+struct hero8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
